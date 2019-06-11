2b7eb8450e8057ce4995cdb07cbbf18e81798adc
yagmurcetintas <yagmurcetin@gmail.com>
2019-06-11 09:48:06 -0400

    added style

--- assets/logic.js
+++ assets/logic.js
@@ -4,7 +4,7 @@ let makeNewButton = function (index, element) {
     $("<button>")
         .text(characters[index])
         .appendTo("#buttons")
-        .addClass("btn btn-sm justify-content-center mr-1 mt-1 userButton")
+        .addClass("btn btn-lg btn-outline-dark justify-content-center mr-1 mt-1 userButton")
         .on("click", characterClicked);
 };
 
@@ -26,15 +26,23 @@ $("#new-show").on("click", function () {
 
 function characterClicked() {
     let clickedButton = $(this).text().replace(/ /g, "+");
+    $(".randomQuotes").addClass("border");
     let queryUrl = "http://api.giphy.com/v1/gifs/search?q=" +
         clickedButton +
         "&api_key=wSCP998sGk4ILdpbjPhlS0EMr9227bNx&limit=10&lang=en";
-        
+
     let randomQuoteUrl = "https://adventure-time-quote-api.glitch.me/api/random";
     $.get(queryUrl).then(function (data) {
         $("#gifs").empty();
         for (let i = 0; i < 10; i++) {
-            let gif = $("<img>").attr("src", data.data[i].images.original.url);
+            console.log(data);
+            let gif = $("<img>")
+            .attr("src", data.data[i].images.original_still.url)
+            .attr("data-still", "true")
+            .attr("data-still-url", data.data[i].images.original_still.url)
+            .attr("data-original-url", data.data[i].images.original.url)
+            .on("click", gifClicked);
+
             let title = $("<p>").text("Title: " + data.data[i].title);
             $("#gifs").append(gif).append(title);
         }
@@ -45,4 +53,16 @@ function characterClicked() {
         $(".randomQuotes").empty();
         $(".randomQuotes").append(randomQuote);
     });
+}
+
+function gifClicked() {
+    let clickedGif = $(this);
+
+    if(clickedGif.attr("data-still") === "true") {
+        clickedGif.attr("data-still", "false");
+        clickedGif.attr("src", clickedGif.attr("data-original-url"));
+    } else {
+        clickedGif.attr("data-still", "true");
+        clickedGif.attr("src", clickedGif.attr("data-still-url"));
+    }
 }
